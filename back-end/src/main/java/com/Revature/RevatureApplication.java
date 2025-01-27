9d3e3aab442143f8952f1cdfcecd2dd2dd886953
Sagar Patel <sagar.patel19nov@gmail.com>
2025-01-27 13:55:28 -0500

    first change

---
+++ back-end/src/main/java/com/Revature/RevatureApplication.java
@@ -0,0 +1,13 @@
+package com.Revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevatureApplication.class, args);
+	}
+
+}
